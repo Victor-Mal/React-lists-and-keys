a4040f68106cec9fab47d3b9df9ed924989bb2eb
Victor Mal <VictorianusVelikyu@gmail.com>
2021-09-29 20:40:05 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    console.log(props.lang);
+  }
+
+  render() {
+        return (
+      <div></div>
+    );
+  }
+}
+
+
+export default App;
